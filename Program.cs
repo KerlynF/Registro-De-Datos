dc66cf68c63aaaa21ba948ab4ea6ce95bde679b3
KerlynF <78883400+KerlynF@users.noreply.github.com>
2021-03-26 22:31:08 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,107 +1,226 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-namespace V1._0._0
+ 
+
+
+namespace Datos_V1._0._0
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string direccion = @"C:\Users\user\Desktop\V1.0.0\datos.txt";
+
             string[] datos = new string[4];
-            string[] header = { "Nombre", "Apellido", "Cedula", "Edad" };
-            string[] outputs = { "Ingrese el nombre", "Ingrese el apellido", "Ingrese la cedula", "Ingrese la edad" };
-            bool confirmacion = false;
-            string datosConfirmacion = "";
+            string[] outputs = new string[4] { "Ingrese el nombre", "Ingrese el apellido", "Ingrese la cedula", "Ingrese la edad" };
+            string[] header = new string[] { "Nombre:", "Apellido:", "Cedula:", "Edad:" };
+            
+            string valorConfirmacion = "";
+            string direccion = args[0];
 
-            for(int x = 0; x < 4; x++)
-            {
-                Console.WriteLine(outputs[x]);
-                datos[x] = Console.ReadLine();
-            }
 
-            Console.WriteLine("¿Desea guardar sus datos? y/n");
+            ProgramManager(datos, outputs, valorConfirmacion, direccion, header);
+            // Aqui abajo terminar
 
-            datosConfirmacion = Console.ReadLine();
-
-            if(datosConfirmacion == "y")
-            {
-                confirmacion = true;
-            }
-            else if(datosConfirmacion == "n")
-            {
-                confirmacion = false;
-            }
-
-            if(confirmacion == true)
-            {
-
-                if(File.Exists(direccion) == true)
-                {
-                    using (StreamWriter writer = new StreamWriter(direccion, true))
-                    {
-                        writer.Write(System.Environment.NewLine);
-
-                        writer.Write(AñadirEspacio(3) + datos[0]);
-
-                        for (int x = 0; x < 3; x++)
-                        {
-                            writer.Write(AñadirEspacio(3 + header[x].Length - datos[x].Length) + datos[x + 1]);
-                        }
-                    }
-                }
-                else
-                {
-                    
-                    
-                        File.WriteAllText(direccion, "");
+           
 
 
 
-                        using (StreamWriter writer = new StreamWriter(direccion, true))
-                        {
-
-                            for(int y = 0; y < 4; y++)
-                            {
-                                writer.Write("   " + header[y]);
-                            }
-                            writer.Write(System.Environment.NewLine);
-
-                            writer.Write(AñadirEspacio(3) + datos[0]);
-
-                            for (int x = 0; x < 3; x++)
-                            {
-                                writer.Write(AñadirEspacio(3 + header[x].Length - datos[x].Length) + datos[x + 1]);
-                            }
-                        }
-                    
-                }
-                
-            }
-            else
-            {
-                Console.WriteLine("Gracias por su visita");
-            }
-
-
-        
         }
 
 
-        public static string AñadirEspacio(int numEspacio)
+        public static string AñadirEspacio(int cantidadEspacios)
         {
-            char[] espacios = new char[numEspacio];
+            char[] y = new char[cantidadEspacios];
+            string resultado = "";
 
-            string cEspacios = "";
-
-            for(int x = 0; x < numEspacio; x++)
+            for(int x = 0; x < cantidadEspacios; x++)
             {
-                espacios[x] = ' ';
-                cEspacios += espacios[x];
+                y[x] = ' ';
+                resultado += y[x];
+            }
+            
+            
+
+            return resultado;
+        }
+
+        public static void IntroducirValores(string[] datoss, string[] outputss, string valorConfirmacionn, string direction, string[]headerss)
+        {
+            
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(outputss[i]);
+                datoss[i] = Console.ReadLine();
             }
 
-            return cEspacios;
+
+            Console.WriteLine("¿Desea que se guarden sus datos? Y/N");
+            valorConfirmacionn = Console.ReadLine();
+
+            if (valorConfirmacionn.ToUpper() == "Y")
+            {
+                EscribirValores(direction, headerss, datoss);
+            }
+            else if (valorConfirmacionn.ToUpper() == "N")
+            {
+                Console.WriteLine("No se guardarán sus datos");
+            }
+
+
+            Console.WriteLine("¿Desea introducir más datos? y/n");
+
+            string reconfirmar = Console.ReadLine();
+
+            if (reconfirmar == "y")
+            {
+                IntroducirValores(datoss, outputss, valorConfirmacionn, direction, headerss);
+            }
+            else if (reconfirmar == "n")
+            {
+                Console.WriteLine("Gracias por su visita");
+                ProgramManager(datoss, outputss, valorConfirmacionn, direction, headerss);
+            }
+
+        }
+
+
+
+
+        public static void EscribirValores(string direccionn, string[]headers, string[] datoss )
+        {
             
+            
+                if (File.Exists(direccionn) == true)
+                {
+
+                    using (StreamWriter writer = new StreamWriter(direccionn, append: true))
+                    {
+
+                        writer.Write(System.Environment.NewLine);
+
+                        writer.Write(AñadirEspacio(3) + datoss[0] + ",");
+                        for (int x = 0; x < 3; x++)
+                        {
+                            writer.Write(AñadirEspacio(3 + headers[x].Length - datoss[x].Length) + datoss[x + 1] + ",");
+
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    File.WriteAllText(direccionn, "");
+
+
+                    using (StreamWriter writer = new StreamWriter(direccionn, append: true))
+                    {
+                        for (int x = 0; x < 4; x++)
+                        {
+                            writer.Write("   " + headers[x]);
+
+                        }
+
+                        writer.Write(System.Environment.NewLine);
+
+                        writer.Write(AñadirEspacio(3) + datoss[0] + ",");
+
+                        for (int y = 0; y < 3; y++)
+                        {
+                            writer.Write(AñadirEspacio(3 + headers[y].Length - datoss[y].Length) + datoss[y + 1] + ",");
+                        }
+
+                    }
+
+                }
+
+            
+           
+            
+              
+            
+
+
+           
+        }
+
+
+        public static void ProgramManager(string[] datoss, string[] outputss, string valorConfirmacionn, string direction, string[]headerss)
+        {
+            string opcionElegida = "";
+            Console.WriteLine("¿Qué acción desea realizar?");
+            Console.WriteLine("Introducir nuevos datos (a)");
+            Console.WriteLine("Busqueda por cédula (b)");
+            Console.WriteLine("Mostrar el registro completo (c)");
+            Console.WriteLine("Salir (d)");
+
+            opcionElegida = Console.ReadLine();
+
+            switch (opcionElegida.ToUpper())
+            {
+                case "A":
+                    IntroducirValores(datoss, outputss, valorConfirmacionn, direction, headerss);
+                break;
+
+                case "B":
+                    BusquedaCedula(direction);
+                break;
+
+                case "C":
+                    MostrarRegistroCompleto(direction);
+                break;
+
+                case "D":
+                    Console.WriteLine("Gracias, vuelva pronto");
+                break;
+            }
+        }
+
+
+        public static void BusquedaCedula(string direction)
+        {
+
+            Console.WriteLine("Introduzca el número de cédula de la persona que desea buscar");
+            string x = Console.ReadLine();
+            int linea = 0;
+            string lineaStr = "";
+            
+
+
+            using (StreamReader reader = new StreamReader(direction))
+            {
+               
+                
+                foreach(string y in File.ReadAllLines(direction))
+                {
+                    lineaStr = y;
+                    
+
+                    linea = reader.ReadLine().IndexOf(x);
+                   
+                    
+
+                    if (linea > -1)
+                    {
+                        Console.WriteLine("Datos encontrados: " + lineaStr);
+                    }
+                }
+               
+
+                
+            }
+        }
+
+        public static void MostrarRegistroCompleto(string direction)
+        {
+            using (StreamReader reader = new StreamReader(direction))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+
+                
+            }
         }
     }
 }
